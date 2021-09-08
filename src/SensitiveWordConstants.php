58e321e156b3ba0f0c2dd6238faa30312e551e3a
lemon_lyue <lemon_lyue@163.com>
2021-09-08 22:00:16 +0800

    feat: 🎸 sensitive words constants

---
+++ src/SensitiveWordConstants.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Lemonlyue\SensitiveWordsFilter;
+
+/**
+ * Constants
+ *
+ * Class SensitiveWordConstants
+ * @package Lemonlyue\SensitiveWordsFilter
+ */
+class SensitiveWordConstants
+{
+    /** @var int low level */
+    public const LEVEL_LOW = 1;
+
+    /** @var int hight level */
+    public const LEVEL_HIGHT = 3;
+
+    /** @var int middle level */
+    public const LEVEL_MIDDLE = 2;
+
+}
