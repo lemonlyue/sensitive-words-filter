53671cc35de2b0aa6da4a2ff762e5a05b128576e
lemon_lyue <lemon_lyue@163.com>
2021-09-08 21:57:54 +0800

    refactor: 💡 file utils

--- src/Utils/File.php
+++ src/Utils/File.php
@@ -1,22 +1,31 @@
 <?php
 
-
-namespace Lemonlyue\SensitiveWordsFilter;
+namespace Lemonlyue\SensitiveWordsFilter\Utils;
 
 
 use Lemonlyue\SensitiveWordsFilter\Exceptions\FileException;
 
-class TxtFile
+/**
+ * Class File
+ * @package Lemonlyue\SensitiveWordsFilter
+ */
+class File
 {
+    /** @var  */
     protected $path;
 
+    /**
+     * File constructor.
+     * @param $filePath
+     */
     public function __construct($filePath)
     {
         $this->path = $filePath;
     }
 
     /**
-     * @desc 获取txt文件每行内容
+     * 获取txt文件每行内容
+     *
      * @throws FileException
      */
     public function getLines()
@@ -24,7 +33,7 @@ class TxtFile
         if(!file_exists($this->path)){
             throw new FileException('File does not exist');
         }
-        $f = fopen($this->path, 'r');
+        $f = fopen($this->path, 'rb');
         try {
             while ($line = fgets($f)) {
                 yield $line;
